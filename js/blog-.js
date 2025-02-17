45ff165eba32fb6c5c3b90a284943509e4b4e4e4
aminur-git <aminur.4mi@gmail.com>
2025-02-17 20:08:02 +0600

    responsive-done

--- js/blog-.js
+++ js/blog-.js
@@ -1,3 +1,6 @@
 document.getElementById("goTo-home").addEventListener("click", function () {
   window.location.href = "./index.html";
 });
+document.getElementById("goTo-home-nav").addEventListener("click", function () {
+  window.location.href = "./index.html";
+});
--- js/home.js
+++ js/home.js
@@ -1,3 +1,6 @@
 document.getElementById("goTo-blog").addEventListener("click", function () {
   window.location.href = "./blog.html";
 });
+document.getElementById("goTo-blog-nav").addEventListener("click", function () {
+  window.location.href = "./blog.html";
+});
